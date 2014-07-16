844222fd00108b89a2c168bcce25f784b92e41da
meetvasu15 <meetvasu15@gmail.com>
2014-07-16 18:19:25 -0500

    introducing search

--- _include/js/main.js
+++ _include/js/main.js
@@ -1,6 +1,7 @@
 var paper;
 var p ;
 var coderect;
+var allrectNodes= new Array();
 function createAllMap(){
 	var mapJsonObj = getMapJson ();
 	var wJsonObj = JSON.parse(wMapStr);
@@ -29,6 +30,7 @@ var mySet = paper.set();
 	for (oneBuildingCount in mapJsonObj){
 		var oneBuilding= mapJsonObj[oneBuildingCount];
 		var oneRect = paper.rect(oneBuilding["x"],oneBuilding["y"],oneBuilding["width"], oneBuilding["height"]).attr({fill: "#FFD390", stroke:"#FFB037"});
+		allrectNodes[oneBuilding["id"]] = oneRect;
 		mySet.push(oneRect);
 		oneRect.node.id=oneBuilding["id"];
 		//labelPath(oneRect, oneBuilding["id"]);
@@ -45,7 +47,7 @@ var mySet = paper.set();
 		//labelPath(oneRect, onePortion["id"]);
 		//mySet.push(labelPath(oneRect, onePortion["id"]));
 	}
-	 coderect = mySet.push( paper.rect(20,166.6479,688.57141, 834.28571).attr({ stroke:"black", fill:"rgb(250, 255, 134)", opacity:"0.2"}));
+	 coderect = mySet.push( paper.rect(20,166.6479,688.57141, 834.28571).attr({ stroke:"none", fill:"rgb(250, 255, 134)", opacity:"0.2"}));
 mySet.draggable();
 	//paper.setViewBox(0,0, 700, 1200);
 }
@@ -175,6 +177,59 @@ viewBox.Y = oY;
         });
 
 		function showSearchResult(infoStr, bldId){
-			var searchedBldElt = paper.getById("A");
-			searchedBldElt.attr({fill: "#FFB037", stroke:""})
+			var searchedBldElt = allrectNodes["A"];
+			searchedBldElt.attr({fill: "#FFB037", stroke:""});
+			}
+		function searchListener(){
+		var srchtext =document.getElementById('searchBoxInput').value ;
+		alert(srchtext);
 		}
+		
+		//type ahead start
+	var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substrRegex;
+ 
+    // an array that will be populated with substring matches
+    matches = [];
+ 
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+ 
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        // the typeahead jQuery plugin expects suggestions to a
+        // JavaScript object, refer to typeahead docs for more info
+        matches.push({ value: str });
+      }
+    });
+ 
+    cb(matches);
+  };
+};
+ 
+var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+];
+ 
+$('.input-group .typeahead').typeahead({
+  hint: true,
+  highlight: true,
+  minLength: 1
+},
+{
+  name: 'states',
+  displayKey: 'value',
+  source: substringMatcher(states)
+});
+	//type ahead end
+		showSearchResult("","");
